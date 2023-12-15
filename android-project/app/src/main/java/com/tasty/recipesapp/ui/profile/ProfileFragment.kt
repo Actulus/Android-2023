34828be081f197b695fcdd0b038750e6a93ce6ba
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2023-12-15 19:16:04 +0200

    working on profile fragment and new recipe part

--- android-project/app/src/main/java/com/tasty/recipesapp/ui/profile/ProfileFragment.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/ui/profile/ProfileFragment.kt
@@ -1,18 +1,25 @@
 package com.tasty.recipesapp.ui.profile
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tasty.recipesapp.R
 import com.tasty.recipesapp.repository.recipe.model.RecipeModel
-//import com.tasty.recipesapp.ui.recipe.RecipesFragment.Companion.BUNDLE_EXTRA_SELECTED_RECIPE_ID
+import com.tasty.recipesapp.repository.recipe.model.RecipeRepository
+import com.tasty.recipesapp.ui.recipe.adapter.RecipesListAdapter
+import com.tasty.recipesapp.ui.recipe.viewmodel.RecipeListViewModel
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private val TAG: String? = RecipeRepository::class.java.canonicalName
 
 class ProfileFragment : Fragment() {
     private var param1: String? = null
@@ -30,8 +37,53 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val addRecipeButton = view.findViewById<View>(R.id.addRecipeButton)
+        addRecipeButton.setOnClickListener {
+            navigateToNewRecipe()
+        }
+        listMyRecipes()
+    }
+
+    private fun navigateToNewRecipe() {
+        try {
+            findNavController().navigate(R.id.action_profileFragment_to_recipeFormFragment)
+        } catch (e: Exception) {
+            Log.d(TAG, "navigateToNewRecipe: $e")
+        }
+    }
+
+    private fun listMyRecipes() {
+        val viewModel = ViewModelProvider(this)[RecipeListViewModel::class.java]
+
+        context?.let {
+            viewModel.fetchRecipeData(it)
+        }
+
+        viewModel.recipesList.observe(viewLifecycleOwner) { recipes ->
+            val adapter = RecipesListAdapter(recipes, requireContext(), onItemClickListener = {
+                navigateToRecipeDetail(it)
+            })
+            val recyclerView = view?.findViewById<RecyclerView>(R.id.recipes_recycler_view)
+            if (recyclerView != null) {
+                recyclerView.adapter = adapter
+            }
+            if (recyclerView != null) {
+                recyclerView.layoutManager = LinearLayoutManager(context)
+            }
+        }
+    }
+
+    private fun navigateToRecipeDetail(recipe: RecipeModel){
+        Log.d(TAG, "navigateToRecipeDetail: $recipe")
+        findNavController().navigate(
+            R.id.action_profileFragment_to_recipeDetailFragment,
+            bundleOf("recipeID" to recipe.recipeID)
+        )
     }
 
     companion object {
--- android-project/app/src/main/java/com/tasty/recipesapp/ui/recipe/RecipeDetailFragment.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/ui/recipe/RecipeDetailFragment.kt
@@ -1,6 +1,5 @@
 package com.tasty.recipesapp.ui.recipe
 
-import android.media.Image
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,14 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import coil.load
 import com.tasty.recipesapp.R
 import com.tasty.recipesapp.repository.recipe.model.RecipeModel
 import com.tasty.recipesapp.repository.recipe.model.RecipeRepository
 import com.tasty.recipesapp.ui.recipe.viewmodel.RecipeDetailViewModel
-import org.w3c.dom.Text
 
 private const val ARG_RECIPE_ID = "recipeID"
 private val TAG: String? = RecipeRepository::class.java.canonicalName
